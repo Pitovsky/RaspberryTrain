79a75e631fd19950d41652d0152b7aeb8db4fac1
peterpitovsky <petr.gezhes@diginetica.com>
2019-10-27 08:18:15 +0100

    traincam frame queue

--- traincam.py
+++ traincam.py
@@ -2,6 +2,7 @@ import sys
 import threading
 import time
 from random import random
+from queue import Queue
 
 import numpy as np
 import cv2
@@ -23,9 +24,12 @@ class TrainCam:
         self.prev_danger = False
         self.running = True
         self.cap = cv2.VideoCapture(video_stream_path)
-        self.graph = tf.get_default_graph() 
+        #self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 0)
+        self.frame_queue = Queue()
+        self.graph = tf.get_default_graph()
         self.model = self.load_model()
-        self.job = threading.Thread(target=self.cam_check)
+        self.frame_processor = threading.Thread(target=self.cam_proc)
+        self.frame_poller = threading.Thread(target=self.cam_poll)
     
     def load_model(self):
         #model = keras.applications.resnet50.ResNet50(weights=None)
@@ -36,13 +40,13 @@ class TrainCam:
         #finetuned_model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
         #finetuned_model.load_weights('classification.h5')
         #return finetuned_model
-        return load_model('classification_79.h5') # works only for keras>=2.3.x
+        return load_model('classification_81.h5') # works only for keras>=2.3.x
     
-    def cam_check(self):
+    def cam_proc(self):
         while self.running:
-            ret, frame = self.cap.read()
-            #cv2.imshow('frame', frame)
-            #k = cv2.waitKey(5) & 0xff
+            frame = self.frame_queue.get()
+            cv2.imshow('frame', frame)
+            k = cv2.waitKey(5) & 0xff
             cur_danger = self.make_danger_prediction(frame)
             print(frame.shape, cur_danger)
             if cur_danger == self.prev_danger:
@@ -55,15 +59,27 @@ class TrainCam:
         with self.graph.as_default():
             preds = self.model.predict(x)
         return np.argmax(preds) != 0 # 0 is 'clear' class
+    
+    def cam_poll(self):
+        while self.running:
+            ret, frame = self.cap.read()
+            if not self.frame_queue.empty():
+                try:
+                    self.frame_queue.get_nowait()   # discard previous (unprocessed) frame
+                except Queue.Empty:
+                    pass
+            self.frame_queue.put(frame)
 
     
     def start(self):
-        self.job.start()
+        self.frame_processor.start()
+        self.frame_poller.start()
     
     def is_dangerous(self):
         return self.danger
     
     def stop(self):
         self.running = False
-        self.job.join()
+        self.frame_processor.join()
+        self.frame_poller.join()
         self.cap.release()
